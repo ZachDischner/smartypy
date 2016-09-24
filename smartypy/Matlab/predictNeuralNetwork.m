2e5fc45cd618aff9f1b08d58c4747ee51fbdc52a
Zach Dischner <zach.dischner@colorado.edu>
2016-09-24 15:43:03 -0600

    Added 2-layer Neural Network training functions and test utilities. Verified against course submission

--- smartypy/Matlab/predictNeuralNetwork.m
+++ smartypy/Matlab/predictNeuralNetwork.m
@@ -45,4 +45,15 @@ h = a3; %possibilities
 % =========================================================================
 
 
+%==========================================================================
+% smaller, less explicit version
+% m = size(X, 1);
+% num_labels = size(Theta2, 1);
+
+% p = zeros(size(X, 1), 1);
+
+% h1 = sigmoid([ones(m, 1) X] * Theta1');
+% h2 = sigmoid([ones(m, 1) h1] * Theta2');
+% [dummy, p] = max(h2, [], 2);
+
 end
