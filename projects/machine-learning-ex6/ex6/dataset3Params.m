169f860fa85fce1a5df881881bbc89d3d0f3e99a
Zach Dischner <zach.dischner@colorado.edu>
2016-10-11 03:39:29 -0600

    Adding lots of MATLAB course project solutions

---
+++ projects/machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,62 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+% Cs = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+% sigmas = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+% err = 100000000;
+% [C1, S1] = meshgrid(Cs, sigmas);
+% errs = zeros(size(C1));
+% 
+% for ci = 1:length(Cs)
+%     for si = 1:length(sigmas)
+%         model = svmTrain(X, y, Cs(ci), @(x1, x2) gaussianKernel(x1, x2, sigmas(si)));
+%         predictions = svmPredict(model, Xval);
+%         this_err = mean(double(predictions ~= yval));
+%         errs(ci,si) = this_err;
+%         if this_err < err
+%             fprintf('lower error! %3.5f C = %d, sigma = %d\n',this_err,Cs(ci),sigmas(si))
+%             err = this_err;
+%             C = Cs(ci);
+%             sigma = sigmas(si);
+%         end
+%     end
+% end
+% 
+% 
+% 
+% % Plot the SVM boundary
+% figure  
+% surf(C1, S1, errs);
+% hold off;
+% xlabel('C'); ylabel('\sigma');zlabel('Error')
+% 
+% [rows,cols]=find(errs==min(errs(:)));
+% C = Cs(rows(1));
+% sigma = sigmas(cols(1));
+
+C = 1.0; sigma = 0.1;
+% =========================================================================
+
+end
