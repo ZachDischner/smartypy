277ba6b36dc61f4e388480a9fd477d34885a2bf0
Zach Dischner <zach.dischner@colorado.edu>
2016-10-15 20:19:24 -0600

    Refactored kmean Centroid() class to transparantly store indices of associated data points, lets us reconstruct original data

--- smartypy/kmeans.py
+++ smartypy/kmeans.py
@@ -33,9 +33,12 @@ Nomenclature:
     See README.md
 
 TODO/Improvements:
+    * Better dedicated 'cohesion' estimate. AKA implement actual cost function()
     * Dedicated KmeansClassifier() class to handle procedural logic
-    * Slow. Some parts are easy to parallelize though without losing the overall class 
-        structure and easiness to follow along
+    * Post-classification analysis of whole centroid set, see if we're underfitting ought to be 
+        pretty easy - average cohesion for a centroid would be high if it is spanning different 
+        distinct clusters. 
+    * Slow. Like holy shit it slow! Mainly associate_centroids(). Can't figure out why or how to speed it up
 
     """
 
@@ -45,10 +48,9 @@ TODO/Improvements:
 import os
 import sys
 import numpy as np
-import pandas as pd
-import pylab as plt
-from numba import jit, njit
+from numba import jit
 import scipy.io as sio
+import profile
 
 ## Local utility module
 _here = os.path.dirname(os.path.realpath(__file__))
@@ -72,16 +74,21 @@ class Centroid(object):
     def cohesion(self):
         """Handy for me, probably not statistically correct"""
         return self._cohesion[-1]
+    
+    @property
+    def associated(self):
+        return self.X[self.associated_ix]
 
-    def __init__(self, x):
+    def __init__(self, X, x):
         """Initialize centroid to n-dimensional `x` """
 
         ## positions -> list of all centroid positions, kept for historcal purposes
         self.positions = [x]
 
         ## associated -> list of associated datapoints
-        self.associated = []
+        self.associated_ix = []
         self._cohesion = [None]
+        self.X = X
     
     def distance(self,x):
         """Compute distance of centroid to `x` datapoint
@@ -93,15 +100,15 @@ class Centroid(object):
     def calc_cohesion(self):
        self._cohesion.append(np.mean([self.distance(x) for x in self.associated]))
     
-    def associate(self,x):
+    def associate(self,ix):
         """Associate datapoint x with centroid object"""
-        self.associated.append(x)
+        self.associated_ix.append(ix)
     
     def update(self):
         """Update current centroid estimate as the average of all assocaited points. Clear assocaited"""
         self.calc_cohesion()
         self.positions.append(np.mean(self.associated,axis=0))
-        self.associated = []
+        self.associated_ix = []
 
 
 ##############################################################################
@@ -113,15 +120,19 @@ def initialize_centroids(X,num):
     m,n = X.shape
     rand_indices = np.random.permutation(m)
 
-    return [Centroid(x) for x in X[rand_indices[0:num]]]
+    return [Centroid(X,x) for x in X[rand_indices[0:num]]]
 
 def associate_centroids(X,centroids):
-    """Associate each datapoint (row of X) with the Centroid() closest to it"""
-    for x in X:
+    """Associate each datapoint (row of X) with the Centroid() closest to it
+    
+    Crazy slow... like way slower than MATLAB. Why is that? Tried about a dozen different 
+    ways to formulate this...
+    """
+    for ix,x in enumerate(X):
         distances = [centroid.distance(x) for centroid in centroids]
         
-        ## Smallest distance gets the point associated! 
-        centroids[np.argmin(distances)].associate(x)
+        # ## Smallest distance gets the point associated! 
+        centroids[np.argmin(distances)].associate(ix)
     return centroids
 
 def update_centroids(centroids):
@@ -141,21 +152,22 @@ def classify_kmeans(X,num,max_iters=10,min_cohesion=0.001):
     prev_cohesion = 1e6
     print("K-means algo grouping dataset into {} features, with maximum iterations {}\n".format(num,max_iters))
     for ii in range(max_iters):
-        print("\rClassification iteration {}, %{:3.2f} Finished".format(ii,float(ii/max_iters)*100), end="")
+        print("\rClassification iteration {}, %{:3.2f} Finished with average cohesion {:3.3}".format(ii,float(ii/max_iters)*100,prev_cohesion), end="")
         centroids = associate_centroids(X,centroids)
         
-        ## Dinky check for convergence
+        ## Dinky check for convergence. Pretty dumb and not well thought out. 
         if ii > 0:
             overall_cohesion = np.mean([c.cohesion for c in centroids])
             delta = abs(prev_cohesion-overall_cohesion)
-            if delta < min_cohesion:
-                print("\nCentroid cohesion is no longer changing. Stopping classification")
-                break
+            if min_cohesion is not None:
+                if delta < min_cohesion:
+                    print("\nCentroid cohesion is no longer changing. Stopping classification")
+                    break
             prev_cohesion = overall_cohesion
     
         if ii < max_iters-1:
             update_centroids(centroids)
-    print("Classification finished with overall average cohesion {:3.3f}".format(overall_cohesion))
+    print("\nClassification finished with overall average cohesion {:3.3f}".format(overall_cohesion))
     return centroids
 
             
